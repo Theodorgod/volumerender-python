fd6470466880eaae959256b173679b4b63216244
OGZ29 <oliver.zhou@outlook.com>
2026-02-13 11:29:55 +0100

    lagt till jämförelser. lagt in in-place operationer för transferFunction

--- volumerender.py
+++ volumerender.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py as h5
 from scipy.interpolate import interpn
-from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
+# from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
 import os
 
 """
@@ -13,31 +13,31 @@ Simulate the Schrodinger-Poisson system with the Spectral method
 """
 
 
-def transferFunction(x):
-    r = (
+def transferFunction(x, rgba):
+    rgba[0] += (
         1.0 * np.exp(-((x - 9.0) ** 2) / 1.0)
         + 0.1 * np.exp(-((x - 3.0) ** 2) / 0.1)
         + 0.1 * np.exp(-((x - -3.0) ** 2) / 0.5)
     )
-    g = (
+    rgba[1] += (
         1.0 * np.exp(-((x - 9.0) ** 2) / 1.0)
         + 1.0 * np.exp(-((x - 3.0) ** 2) / 0.1)
         + 0.1 * np.exp(-((x - -3.0) ** 2) / 0.5)
     )
-    b = (
+    rgba[2] += (
         0.1 * np.exp(-((x - 9.0) ** 2) / 1.0)
         + 0.1 * np.exp(-((x - 3.0) ** 2) / 0.1)
         + 1.0 * np.exp(-((x - -3.0) ** 2) / 0.5)
     )
-    a = (
+    rgba[3] += (
         0.6 * np.exp(-((x - 9.0) ** 2) / 1.0)
         + 0.1 * np.exp(-((x - 3.0) ** 2) / 0.1)
         + 0.01 * np.exp(-((x - -3.0) ** 2) / 0.5)
     )
 
-    return r, g, b, a
 
 
+# @profile
 def main():
     """Volume Rendering"""
 
@@ -56,9 +56,9 @@ def main():
     images = []
     
     # Do Volume Rendering at Different Viewing Angles
-    Nangles = 50
+    Nangles = 10
     for i in range(Nangles):
-        print("Rendering Scene " + str(i + 1) + " of " + str(Nangles) + ".\n")
+        # print("Rendering Scene " + str(i + 1) + " of " + str(Nangles) + ".\n")
 
         # Camera Grid / Query Points -- rotate camera view
         angle = np.pi / 2 * i / Nangles
@@ -77,7 +77,15 @@ def main():
         image = np.zeros((camera_grid.shape[1], camera_grid.shape[2], 3))
 
         for dataslice in camera_grid:
-            r, g, b, a = transferFunction(np.log(dataslice))
+            rgba = np.zeros((4,) + dataslice.shape)
+            transferFunction(np.log(dataslice), rgba)
+            r = rgba[0]
+            g = rgba[1]
+            b = rgba[2]
+            a = rgba[3]
+
+
+            # r, g, b, a = transferFunction(np.log(dataslice))
             image[:, :, 0] = a * r + (1 - a) * image[:, :, 0]
             image[:, :, 1] = a * g + (1 - a) * image[:, :, 1]
             image[:, :, 2] = a * b + (1 - a) * image[:, :, 2]
@@ -86,19 +94,28 @@ def main():
         
         # Store image and save as PNG
         images.append((image * 255).astype(np.uint8))
-        plt.imsave(f"volumerender{i}.png", image)
+        # plt.imsave(f"volumerender{i}.png", image)
 
     # Create GIF from the rendered images
-    print("Creating GIF from rendered images...\n")
-    clip = ImageSequenceClip(images, fps=60)
-    clip.write_gif("volumerender.gif")
-    print("GIF saved as volumerender.gif\n")
+    # print("Creating GIF from rendered images...\n")
+    # clip = ImageSequenceClip(images, fps=60)
+    # clip.write_gif("volumerender.gif")
+    # print("GIF saved as volumerender.gif\n")
     
     # Open the GIF with default viewer
-    os.startfile("volumerender.gif")
+    # os.startfile("volumerender.gif")
+    images_array = np.array(images)
+    if os.path.exists("volumerender_images_new.npy"):
+        os.remove("volumerender_images_new.npy")
+    np.save("volumerender_images_new.npy", images_array)
+
 
     return 0
 
 
 if __name__ == "__main__":
     main()
+    old = np.load("volumerender_images_old.npy")
+    new = np.load("volumerender_images_new.npy")
+
+    print(np.array_equal(old, new))
